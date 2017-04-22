0ae39636786fc9b821cc3b68b2c902f0f722115a
SHZ22 <sh1709@live.mdx.ac.uk>
2017-04-22 22:32:47 +0100

    Home Page
    Added home page

---
+++ home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="home.css">
+</head>
+<body>
+  	 <ul>
+  		<li><a href="index.php">Home</a></li>
+ 	    <li><a href="#">News</a></li>
+        <li><a href="#">Contact</a></li>
+        <li><a href="#">About</a></li>
+</ul>
+ 
+  <h3>Welcome!</h3>
+  <h4>Write Your First Blog Here!</h4>
+   <textarea>Write Your First Blog Here</textarea>
+      <input type="submit" name="ctbtn" value="Save">
+      <input type="submit" name="upload" value="Upload Image">
+</body>
+</html>
